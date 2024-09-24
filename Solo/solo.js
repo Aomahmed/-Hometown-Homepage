b726fc35f3294384af7b770d95a03f429cdbfce7
Aomahmed <amahmoud949494@gmail.com>
2024-09-24 21:23:03 +0100

    Add files via upload

---
+++ Solo/solo.js
@@ -0,0 +1,27 @@
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'https://www.youtube.com/watch?v=yKTVqPaNJIw&pp=ygULTWFra2FoIGNpdHk%3D');
+new_a.innerText = 'Makkah';
+
+new_li.appendChild(new_a);
+
+element_list.appendChild(new_li);
+let element_list2 = document.getElementById('list');
+let new_li2 = document.createElement('li');
+let new_a2 = document.createElement('a');
+new_a2.setAttribute("href", 'https://www.youtube.com/watch?v=-ht6pLR3vcY&pp=ygULUml5YWRoIGNpdHk%3D');
+new_a2.innerText = 'Riyadh';
+
+new_li2.appendChild(new_a2);
+
+element_list2.appendChild(new_li2);
+let element_list3 = document.getElementById('list');
+let new_li3 = document.createElement('li');
+let new_a3 = document.createElement('a');
+new_a3.setAttribute("href", 'https://www.youtube.com/watch?v=nrEXm8GgziA&pp=ygULamVkZGFoIGNpdHk%3D');
+new_a3.innerText = 'Jeddah';
+
+new_li3.appendChild(new_a3);
+
+element_list3.appendChild(new_li3);
